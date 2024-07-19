d25e8b57d7d2bda8e76973b401e7b43732ac97e1
Sebil Alagoz <s.alagoz@outlook.fr>
2024-07-19 20:00:31 +0200

    ajout des id de la navBar sans les sections de la page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -27,7 +27,7 @@ const Page = () => {
         <Slider />
       </section>
       <section className="ServicesContainer">
-        <h2 className="Title">Nos services</h2>
+        <h2 className="Title" id="nos-services">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
           <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
@@ -56,11 +56,11 @@ const Page = () => {
         </div>
       </section>
       <section className="EventsContainer">
-        <h2 className="Title">Nos réalisations</h2>
+        <h2 className="Title" id="nos-realisations">Nos réalisations</h2>
         <EventList />
       </section>
       <section className="PeoplesContainer">
-        <h2 className="Title">Notre équipe</h2>
+        <h2 className="Title" id="notre-equipe">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
           <PeopleCard
@@ -96,7 +96,7 @@ const Page = () => {
         </div>
       </section>
       <div className="FormContainer" id="contact">
-        <h2 className="Title">Contact</h2>
+        <h2 className="Title" id="contact">Contact</h2>
         <Modal
           Content={
             <div className="ModalMessage--success">
