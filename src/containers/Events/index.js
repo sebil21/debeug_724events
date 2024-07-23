a994a0cfdec6fc71b821dd16872e9a1368ee5e81
Sebil Alagoz <s.alagoz@outlook.fr>
2024-07-23 13:55:43 +0200

    filtre de la section events OK, il manque la maj des dates

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,15 +14,31 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
 
-  const filteredEvents = (data?.events || [])
-		.filter(event => !type || event.type === type)
-		.slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
+  const filteredEvents = (
+    (!type
+      ? data?.events
+      : data?.events.filter((event) => event.type === type)) || [] // ajout de .filter
+  )
+    
+    .filter((event, index) => {
+      if (
+        (currentPage - 1) * PER_PAGE <= index &&
+        PER_PAGE * currentPage > index
+      ) {
+        return true;
+      }
+
+      return false 
+    })
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
