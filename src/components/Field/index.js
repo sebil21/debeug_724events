1066fca6c7b759d438d65dfa95ef0ea491c41a90
Sebil Alagoz <s.alagoz@outlook.fr>
2024-09-24 21:24:28 +0200

    nouveau repository, beug sur l'ancien

---
+++ src/components/Field/index.js
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+
+import "./style.scss";
+
+export const FIELD_TYPES = {
+  INPUT_TEXT: 1,
+  TEXTAREA: 2,
+};
+
+const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder, required = false }) => {
+  let component;
+  switch (type) {
+    case FIELD_TYPES.INPUT_TEXT:
+      component = (
+        <input
+          type="text"
+          name={name}
+          placeholder={placeholder}
+          data-testid="field-testid"
+          required={required}
+        />
+      );
+      break;
+    case FIELD_TYPES.TEXTAREA:
+      component = <textarea name={name} data-testid="field-testid" required={required} />;
+      break;
+    default:
+      component = (
+        <input
+          type="text"
+          name={name}
+          placeholder={placeholder}
+          data-testid="field-testid"
+          required={required}
+
+        />
+      );
+  }
+  return (
+    <div className="inputField">
+      <span>{label}</span>
+      {component}
+    </div>
+  );
+};
+
+Field.propTypes = {
+  type: PropTypes.oneOf(Object.values(FIELD_TYPES)),
+  name: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  required: PropTypes.string.isRequired,
+
+};
+ Field.defaultProps = {
+   label: "",
+   placeholder: "",
+   type: FIELD_TYPES.INPUT_TEXT,
+   name: "field-name",
+ }
+
+export default Field;
